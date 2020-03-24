4c9c830f758690017e4b5e48e54dd1ec9147bed2
Christian Jäger <mail@plainoldcj.com>
2020-03-24 20:01:44 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,90 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import { gql } from 'apollo-boost';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+	uri: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// HSL:1203422
+// HSL:1203406
+
+function formatTime(realtimeDeparture, serviceDay)
+{
+	// var date = new Date((realtimeDeparture + serviceDay)*1000);
+
+	// // Hours part from the timestamp
+	// var hours = date.getHours();
+	// // Minutes part from the timestamp
+	// var minutes = "0" + date.getMinutes();
+	// // Seconds part from the timestamp
+	// var seconds = "0" + date.getSeconds();
+
+	// // Will display time in 10:30:23 format
+	// var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+	// var ms = date - new Date();
+
+	var ms = (realtimeDeparture + serviceDay)*1000 - Date.now();
+
+	return Math.floor(ms / (1000 * 60));
+}
+
+function Hsl(props)
+{
+	var stopname = props.value;
+
+	var HSL_QUERY = gql`
+	{
+		stop(id: "${stopname}") {
+			name
+				stoptimesWithoutPatterns {
+					scheduledArrival
+						realtimeArrival
+						arrivalDelay
+						scheduledDeparture
+						realtimeDeparture
+						departureDelay
+						realtime
+						realtimeState
+						serviceDay
+						headsign
+				}
+		}
+	}
+	`;
+
+	const { loading, error, data } = useQuery(HSL_QUERY);
+
+	if(loading) return <p>Loading...</p>;
+	if(error) return <p>Error</p>;
+
+	return data.stop.stoptimesWithoutPatterns.map(({ realtimeDeparture, serviceDay, headsign }) => (
+		<div>
+			<p>
+				{headsign}: {formatTime(realtimeDeparture, serviceDay)}min
+			</p>
+		</div>
+	));
+}
+
+const App = () => (
+	<ApolloProvider client={client}>
+		<p>
+			<Hsl value="HSL:1203422" />
+		</p>
+		----------------
+		<p>
+			<Hsl value="HSL:1203406" />
+		</p>
+	</ApolloProvider>
+);
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
+
